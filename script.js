5c0a2f502deb46c2f54eb5de803f70f761ffa649
mahemoud2000 <140842379+mahemoud2000@users.noreply.github.com>
2024-11-05 10:04:29 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+let title = "Elzero",description = "Elzero Web School", date = "25/10",contains = `<div class="card"><h3>${title}</h3> <p>${description}</p> <span>${date}</span></div>`;
+document.write(contains.repeat(4));
